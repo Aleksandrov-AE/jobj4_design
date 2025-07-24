adef4aaf35e884c2598608e426eb11f94ff9bf03
Aleksandrov-AE <interfanspb@gmail.com>
2025-07-24 20:06:03 +0300

    1. Связи между таблицами [#373615]

--- src/main/java/ru/job4j/db/script.sql
+++ src/main/java/ru/job4j/db/script.sql
@@ -1,42 +1,43 @@
-REATE TABLE author (
-  id INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE author(
+  id SERIAL INT PRIMARY KEY,
   name VARCHAR(100)
 );
 
-CREATE TABLE book (
-  id INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE book(
+  id SERIAL INT PRIMARY KEY,
   title VARCHAR(200),
   author_id INT,
   FOREIGN KEY (author_id) REFERENCES author(id)
 );
 
-CREATE TABLE person (
-  id INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE person(
+  id SERIAL PRIMARY KEY,
   name VARCHAR(100)
 );
 
-CREATE TABLE passport (
-  id INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE passport(
+  id SERIAL PRIMARY KEY,
   number VARCHAR(20) UNIQUE,
   person_id INT UNIQUE,
   FOREIGN KEY (person_id) REFERENCES person(id)
 );
 
-CREATE TABLE student (
-  id INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE student(
+  id SERIAL PRIMARY KEY,
   name VARCHAR(100)
 );
 
 CREATE TABLE course (
-  id INT PRIMARY KEY AUTO_INCREMENT,
+  id SERIAL PRIMARY KEY,
   title VARCHAR(100)
 );
 
 -- Промежуточная таблица для связи многие-ко-многим:
-CREATE TABLE student_course (
+CREATE TABLE student_course(
   student_id INT,
   course_id INT,
   PRIMARY KEY(student_id, course_id),
   FOREIGN KEY (student_id) REFERENCES student(id),
   FOREIGN KEY (course_id) REFERENCES course(id)
 );
+
