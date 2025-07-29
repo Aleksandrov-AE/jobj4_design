68c61c1546a159ba592e4489656a57f4e0b59029
Aleksandrov-AE <interfanspb@gmail.com>
2025-07-29 13:05:38 +0300

    3. Хранимые процедуры и функции [#504804]

---
+++ src/main/java/ru/job4j/db/procedure.sql
@@ -0,0 +1,40 @@
+create table products
+(
+    id       serial primary key,
+    name     varchar(50),
+    producer varchar(50),
+    count    integer default 0,
+    price    integer
+);
+
+CREATE OR REPLACE PROCEDURE delete_product_by_id(pid INT)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    DELETE FROM products
+    WHERE id = pid;
+END;
+$$;
+
+
+CALL delete_product_by_id(3);
+
+
+CREATE OR REPLACE FUNCTION delete_zero_count_products()
+RETURNS INTEGER
+LANGUAGE plpgsql
+AS $$
+DECLARE
+    deleted_count INT;
+BEGIN
+    DELETE FROM products
+    WHERE count = 0
+    RETURNING * INTO deleted_count;
+
+    GET DIAGNOSTICS deleted_count = ROW_COUNT;
+
+    RETURN deleted_count;
+END;
+$$;
+
+SELECT delete_zero_count_products();
