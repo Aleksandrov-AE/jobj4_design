0c9b036e677295127183b06d24f4d67daaa87ddb
Aleksandrov-AE <interfanspb@gmail.com>
2025-07-30 22:51:14 +0300

    5. Курсор в SQL [#504816]

---
+++ src/main/java/ru/job4j/db/cursor.sql
@@ -0,0 +1,22 @@
+BEGIN;
+
+DECLARE products_cursor SCROLL CURSOR FOR
+SELECT * FROM products;
+
+
+FETCH LAST FROM products_cursor;
+
+MOVE BACKWARD 15 FROM products_cursor;
+FETCH FORWARD 0 FROM products_cursor;
+
+MOVE BACKWARD 8 FROM products_cursor;
+FETCH FORWARD 0 FROM products_cursor;
+
+MOVE BACKWARD 5 FROM products_cursor;
+FETCH FORWARD 0 FROM products_cursor;
+
+MOVE BACKWARD 1 FROM products_cursor;
+FETCH FORWARD 0 FROM products_cursor;
+
+CLOSE products_cursor;
+COMMIT;
