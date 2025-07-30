12cd74103b02a82d6fca98a829076a86c70857a4
Aleksandrov-AE <interfanspb@gmail.com>
2025-07-30 16:00:47 +0300

    4.4. Транзакции. Работа с транзакциями в PostgreSQL [#504813]

---
+++ src/main/java/ru/job4j/db/trasactionPg.sql
@@ -0,0 +1,7 @@
+BEGIN;
+INSERT INTO products (name, stock) VALUES ('grape', 50);
+SAVEPOINT sp1;
+UPDATE products SET stock = 999 WHERE name = 'apple';
+ROLLBACK TO SAVEPOINT sp1;
+UPDATE products SET stock = 777 WHERE name = 'banana';
+COMMIT;
